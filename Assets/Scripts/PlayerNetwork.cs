131cf367bcebcec5bff193ae3b0abcbf706d7dac
JohnyTes <j3_horak@utb.cz>
2024-10-05 21:05:33 +0200

    34:00

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData{
+            _int = 52,
+            _bool = true,
+        }, NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+    public struct MyCustomData : INetworkSerializable
     {
-        
+        public int _int;
+        public bool _bool;
+        public string message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue)=>{
+            Debug.Log(OwnerClientId+"; "+newValue._int+"; "+newValue._bool+"; "+newValue.message);
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!IsOwner)
+        {
+            return;
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            randomNumber.Value = new MyCustomData{
+                _int=10,
+                _bool=false,
+                message="hello",
+            };
+        }
+        Vector3 moveDir = new Vector3(0,0,0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDir.z = + 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDir.z = - 1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDir.x = - 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDir.x = + 1f;
+        }
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 }
